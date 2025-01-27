362bdea81ff3ce8fdafd015cff4cc074d2d54176
Mohamed Alwakil <736736+MohamedDiaa@users.noreply.github.com>
2025-01-27 20:43:49 +0100

    Initial Commit

---
+++ TextArtReplica/TextArtReplicaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextArtReplicaApp.swift
+//  TextArtReplica
+//
+//  Created by Mohamed Alwakil on 2025-01-27.
+//
+
+import SwiftUI
+
+@main
+struct TextArtReplicaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
